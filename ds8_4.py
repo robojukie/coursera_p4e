8e925cc519a394059c515848df7229e23d871cae
Julie Paik <juliep@Julies-MBP.attlocal.net>
2016-03-07 21:41:08 -0600

    descriptions

--- ds8_4.py
+++ ds8_4.py
@@ -1,3 +1,9 @@
+# 8.4 Open the file romeo.txt and read it line by line. For each line, split the
+# line into a list of words using the split() method. The program should build a
+# list of words. For each word on each line check to see if the word is already
+# in the list and if not append it to the list. When the program completes, sort
+# and print the resulting words in alphabetical order.
+
 fname = raw_input("Enter file name: ")
 fh = open(fname)
 lst = list()
@@ -17,24 +23,3 @@ for line in fh:
 lst.sort()
 # print list
 print lst
-
-
-# 
-# fname = raw_input("Enter file name: ")
-# fh = open(fname)
-# lst = list()
-# for line in fh:
-#     #split line into list of words
-#     elems = line.split()
-#     # append words not in list
-# #     for i in range(len(elems)):
-# #         if elems[i] not in lst:
-# # 	 	  lst.append(elems[i])
-#     for word in elems :
-#         if word not in lst :
-#             lst.append(word)
-#         
-# # sort list
-# lst.sort()
-# # print list
-# print lst
